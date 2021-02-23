7f36094109720ecb9a386a0c75c8774a68ea73e2
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-02-23 08:26:20 -0700

    Update solutions-challenge15.cs

--- solutions-challenge15.cs
+++ solutions-challenge15.cs
@@ -8,32 +8,38 @@ namespace learning
     {
         public static class Global
         {
-            public static List<string> winning_plays = new List<string>();
+            public static List<string> winning_plays = new List<string> { "rock", "paper", "scissors" };
         }
         static string oneGame()
         {
-            //play a round
             Random rnd = new Random();
             string win = "";
-            List<string> plays = new List<string> { "rock", "paper", "scissors" };
-            string c_play = plays[rnd.Next(0, 2)];
-            Console.Write("\n\nChoose 'R', 'P', or 'S' (rock, paper, or scissors): ");
+
+            //List<string> plays = new List<string> { "rock", "paper", "scissors" };
+            int choice = rnd.Next(0, Global.winning_plays.Count);
+            //string c_play = plays[rnd.Next(0, 2)];
+            string c_play = Global.winning_plays[choice];
+
+            Console.Write("\n\nChoose rock, paper, or scissors: ");
             string u_play = Console.ReadLine();
 
-            if (u_play.ToLower() == "r")
+            if (u_play.ToLower().StartsWith("r"))
             {
                 if (c_play == "rock") { win = "tie"; } else if (c_play == "paper") { win = "computer"; } else if (c_play == "scissors") { win = "player"; }
             }
-            else if (u_play.ToLower() == "p")
+            else if (u_play.ToLower().StartsWith("p"))
             {
                 if (c_play == "paper") { win = "tie"; } else if (c_play == "scissors") { win = "computer"; } else if (c_play == "rock") { win = "player"; }
             }
-            else if (u_play.ToLower() == "s")
+            else if (u_play.ToLower().StartsWith("s"))
             {
                 if (c_play == "scissors") { win = "tie"; } else if (c_play == "rock") { win = "computer"; } else if (c_play == "paper") { win = "player"; }
             }
+
             Console.WriteLine("Computer chose " + c_play);
+            if (win == "computer") { Global.winning_plays.Add(c_play); }
             if (win != "tie") { Console.WriteLine("Win goes to " + win); } else { Console.WriteLine("Game is a tie"); }
+
             return win;
         }
         static void Main(string[] args)
@@ -54,9 +60,9 @@ namespace learning
             }
             Console.Write("Computer score: " + comp_score + " Player score: " + player_score);
 
-            Console.Write("\nPlay again? (Y/N): ");
+            Console.Write("\nPlay again? Yes or no: ");
             again_string = Console.ReadLine();
-            if (again_string.ToLower() == "y")
+            if (again_string.ToLower().StartsWith("y"))
             {
                 again = true;
             }
@@ -78,9 +84,9 @@ namespace learning
                         comp_score += 1;
                     }
                     Console.Write("Computer score: " + comp_score + " Player score: " + player_score);
-                    Console.Write("\nPlay again? (Y/N): ");
+                    Console.Write("\nPlay again? Yes or no: ");
                     again_string = Console.ReadLine();
-                    if (again_string.ToLower() == "y")
+                    if (again_string.ToLower().StartsWith("y"))
                     {
                         again = true;
                     }
@@ -94,5 +100,3 @@ namespace learning
         }
     }
 }
-
-
